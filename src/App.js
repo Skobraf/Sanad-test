e95a90bb1786b94609238a622bab9c1f0292081a
skobraf <skobraf@gmail.com>
2019-12-09 18:46:01 +0000

    added css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { removeIn, setIn, updateIn } from 'immutable';
+
+import Form from './Form/Form';
 import './App.css';
 
+
+function removeChild( source, parents, id ) {
+    const path = parents.reduce( ( res, id ) => {
+        res.push( id, 'children' );
+        return res;
+    }, [] );
+
+    return removeIn( source, [ ...path, id ] );
+}
+
+function addChild( source, parents, item ) {
+    const path = parents.reduce( ( res, id ) => {
+        res.push( id, 'children' );
+        return res;
+    }, [] );
+
+    return setIn( source, [ ...path, item.id ], item.value );
+}
+
+function updateChild( source, parents, id, data ) {
+    const path = parents.reduce( ( res, id ) => {
+        res.push( id, 'children' );
+        return res;
+    }, [] );
+
+    return updateIn( source, [ ...path, id ], ( val ) => ( {
+        ...val,
+        ...data,
+    } ) );
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ tree, setTree ] = useState( {} );
+
+    const addItem = ( parents = [] ) => {
+        const id = Math.random().toString( 36 ).substr( 2, 9 );
+        const item = {
+            id,
+            parents,
+            name: '',
+            type: '',
+            value: null,
+            children: {},
+        };
+
+        setTree( ( tree ) => {
+            return addChild( tree, parents, {
+                id,
+                value: item,
+            } );
+        } );
+    };
+
+
+    const removeItem = ( parents, id ) =>  {
+        setTree( (tree) => {
+            return removeChild( tree, parents, id );
+        } );
+    };
+
+    const updateItem = ( parents, id, values ) => {
+        setTree( ( tree ) => {
+            return updateChild( tree, parents, id, values );
+        });
+    };
+
+    const handleAddItem = ( e ) => {
+        e.preventDefault();
+        addItem();
+    };
+
+    return (
+        <div>
+            {Object.entries( tree ).map( ( [ key, data ] ) => (
+                <Form
+                    key={key}
+                    item={data}
+                    addItem={addItem}
+                    updateItem={updateItem}
+                    removeItem={removeItem}
+                />
+            ) )}
+            <button type="button" onClick={handleAddItem}>Add new</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Form/Form.js
@@ -0,0 +1,119 @@
+import React, { useRef, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import  './form.css';
+
+function Form({ item, ...props }) {
+    
+    const [ visible, setVisible ] = useState(false);
+    const typeRef = useRef();
+    const textRef = useRef();
+    const valueRef = useRef();
+
+    useEffect(() => {
+        textRef.current.focus();
+    }, []);
+
+    const losingFocus = ( e ) => {
+        if( e.target.value === '' ) {
+            e.target.style.border = ' 2px solid red';
+        } else {
+            e.target.style = 'none';
+        }
+    };
+    const toggleVisibility = (value) => {
+        setVisible(value === 'array' || value === 'structure'); 
+    };
+
+    const addItem = e => {
+        e.preventDefault();
+        const { id, parents } = item;
+
+        props.addItem( [ ...parents, id ] );
+    };
+
+    const removeItem = e => {
+        e.preventDefault();
+        const { id, parents } = item;
+
+        props.removeItem( parents, id );
+    };
+
+    const updateItem = () => {
+        const { id, parents } = item;
+        const value = typeRef.current.value === 'array' || typeRef.current.value === 'structure' ? null :  valueRef.current.value;
+        const values = {
+            type: typeRef.current.value,
+            text: textRef.current.value,
+            value, 
+        };
+
+        props.updateItem( parents, id, values );
+    };
+
+    const handleSelectChange = (e) => {
+        const { value } = e.target;
+
+        toggleVisibility(value);
+        updateItem();
+    }; 
+
+    const { children } = item;
+    return (
+        <ul>
+            <li>
+                <form>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        ref={textRef}
+                        onChange={updateItem}
+                        onBlur={losingFocus}
+                    />
+                    <select
+                        name="valeur"
+                        ref={typeRef}
+                        onChange={handleSelectChange}
+                    >
+                        <option value="text">text</option>
+                        <option value="boolean">Boolean</option>
+                        <option value="number">Number</option>
+                        <option value="structure">Structure</option>
+                        <option value="array">Array</option>
+                        <option value="date">Date</option>
+                    </select>
+                    {
+                        visible &&
+                        <button onClick={addItem}>Add</button>
+                        ||
+                        <input
+                            type="text"
+                            placeholder="Value"
+                            ref={valueRef}
+                            onChange={updateItem}
+                        />
+                    }
+                    <button onClick={removeItem}>remove</button>
+                </form>
+            </li>
+            <ul>
+                {Object.entries( children ).map( ( [ key, data ] ) => (
+                    <Form
+                        key={key}
+                        item={data}
+                        addItem={props.addItem}
+                        updateItem={props.updateItem}
+                        removeItem={props.removeItem}
+                    />
+                ) )}
+            </ul>
+        </ul>
+    );
+}
+Form.propTypes = {
+    item: PropTypes.object.isRequired,
+    addItem: PropTypes.func.isRequired,
+    updateItem: PropTypes.func.isRequired,
+    removeItem: PropTypes.func.isRequired,
+};
+export default Form;
